0a209a81df1e427ff3c2ec4b0f0d1fb8aeac69d4
chaitu-mac <kc.kalavacharla@gmail.com>
2015-10-19 00:09:56 -0400

    TestCargo

---
+++ src/main/java/com/cakec/fos/domain/TestCargo.java
@@ -0,0 +1,19 @@
+package com.cakec.fos.domain;
+
+public class TestCargo {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public TestCargo(String name) {
+		super();
+		this.name = name;
+	}
+	
+}
